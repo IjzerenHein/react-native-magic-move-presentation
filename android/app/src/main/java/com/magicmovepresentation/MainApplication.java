5ed23f82a3f0ce69705a6aff48ff7e9c094079eb
Hein Rutjes <hrutjes@gmail.com>
2019-04-17 12:32:15 +0200

    Added react-native-video dependency

--- android/app/src/main/java/com/magicmovepresentation/MainApplication.java
+++ android/app/src/main/java/com/magicmovepresentation/MainApplication.java
@@ -3,6 +3,7 @@ package com.magicmovepresentation;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.ijzerenhein.magicmove.ReactMagicMovePackage;
 import com.reactnativecommunity.statusbar.RNCStatusBarPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new ReactMagicMovePackage(),
             new RNCStatusBarPackage()
       );
